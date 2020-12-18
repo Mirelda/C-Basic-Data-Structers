e7f51ebfa3c3d88bcbfc1562fd26fda64f3137d1
Mirelda <58131945+Mirelda@users.noreply.github.com>
2020-12-18 22:20:08 +0300

    Created basic linked list

---
+++ main.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct node
+{
+    char* name;
+    int number;
+    struct node* next;
+    
+}node;
+
+//gezinme
+void traverse(node* pNode){
+    printf("3 \n");
+    node* temp = pNode;
+    while(temp != NULL)
+    {
+        printf("Name: %s, Number: %d \n",temp->name , temp->number);
+        temp = temp->next;
+    }
+    
+}
+
+//ekleme
+void append(node* root, node* pNode){
+    printf("2 \n");
+    node* temp = root;
+    while (temp->next != NULL){
+        temp = temp->next;
+    }
+
+    temp->next = pNode;
+    
+}
+
+node* createNode(char* name, int number){
+    printf("1 \n");
+    node* pNode = malloc(sizeof(node));
+    pNode->name = name;
+    pNode->number = number;
+    pNode->next = NULL;
+    return pNode;
+}
+
+
+
+
+int main(){
+    
+    node* a = createNode("Sev",1);
+    node* b = createNode("Sevgi",2);
+    node* c = createNode("Sevgili",3);
+    node* d = createNode("Sevilmek",4);
+
+   
+
+    append(a, b);
+    append(a, c);
+    append(a, d);
+
+
+
+    traverse(a);
+
+    return 0;
+}
